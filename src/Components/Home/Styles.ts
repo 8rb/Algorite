57c17594e5c23b83735cf14b2116f6676c5e69ad
TheRoro <39977138+TheRoro@users.noreply.github.com>
2021-09-27 08:34:07 -0500

    🔥 Initialized Next.js App
    Removed old files

--- src/Components/Home/Styles.ts
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const HomeContainer = styled.div`
-    height: 100vh;
-    width: 100%;
-    background-color: #1C2535;
-`
--- src/Styles.ts
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const Components = styled.div`
-    height: 100vh;
-    width: 90%;
-    float:right;
-`
