c3678482caad9aace9810040e5c946204b50d502
TheRoro <39977138+TheRoro@users.noreply.github.com>
2022-01-11 10:34:13 -0500

    ⬆️ Update App to TypeScript

---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+	return (
+		<div className='min-h-screen flex flex-col justify-center items-center'>
+			<h1 className='text-4xl font-bold underline text-white'>Hello world!</h1>
+		</div>
+	)
+}
